1ecc0a0e6ba2895385e8353373618078f425fb1f
MeetGangani <meetgangani56@gmail.com>
2024-08-06 13:53:44 +0530

    CRUD

---
+++ E Commerce/Craftelio.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Craftelio.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ E Commerce/E Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,108 @@
+﻿
+using Craftelio.DataAccess.Data;
+using Craftelio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ToasterMessage = TempData["ToasterMessage"];
+            ViewBag.MessageType = TempData["MessageType"]; // Set the message type
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "DisplayOrder cannot match with name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["ToasterMessage"] = "Category created successfully!";
+                TempData["MessageType"] = "success"; // Set message type to 'success'
+                return RedirectToAction("Index");
+            }
+            TempData["MessageType"] = "error"; // Set message type to 'error'
+            return View();
+        }
+
+        public IActionResult EditCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult EditCategory(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["ToasterMessage"] = "Category updated successfully!";
+                TempData["MessageType"] = "success"; // Set message type to 'success'
+                return RedirectToAction("Index");
+            }
+            TempData["MessageType"] = "error"; // Set message type to 'error'
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["ToasterMessage"] = "Category deleted successfully!";
+            TempData["MessageType"] = "success"; // Set message type to 'success'
+            return RedirectToAction("Index");
+        }
+    }
+}
